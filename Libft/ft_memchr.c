b3c0dcc0f64f165b0f0f1f44e9d34723d647462b
daniel <daniel-congo2@hotmail.com>
2024-10-05 20:55:57 +0200

    ajout memchr.c

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -14,25 +14,23 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t				i;
 	unsigned const char	*str;
 
 	str = (unsigned const char *)s;
 	i = 0;
-
 	while (i < n)
 	{
 		if (str[i] == (unsigned char)c)
-			return ((void *)str);
+			return ((void *)str + i);
 		i++;
 	}
 	return (0);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	const char	*str = "Hello";
-	printf("%s\n", (unsigned char *)ft_memchr(str, 'l', 5));
+	printf("%s\n", (unsigned char *)ft_memchr(str, 'e', 5));
 	return (0);
-}
-
+}*/
--- Libft/libft.h
+++ Libft/libft.h
@@ -13,7 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stdio.h> //printf() ATTENTION A SUPPRIMER AVANT DE RENDRE PROJET CAR PAS AUTORISE
+# include <stdio.h> //printf() ATTENTION A SUPPRIMER CAR PAS AUTORISE
 # include <stddef.h>//size_t
 # include <stdlib.h>//malloc(), free()
 # include <unistd.h>//write()
@@ -24,6 +24,7 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
