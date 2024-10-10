02acf5985b0884f7ad90283b77c22cd5000b86ad
daniel <daniel-congo2@hotmail.com>
2024-10-10 15:48:15 +0200

    ajout ft_strjoin.c

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daafonso <daafonso@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 19:27:09 by daafonso          #+#    #+#             */
-/*   Updated: 2024/10/08 20:08:36 by daafonso         ###   ########.fr       */
+/*   Updated: 2024/10/10 14:15:26 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
+	size_t	s_len;
 
-	str = malloc(sizeof(char) * len + 1);
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	str = malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
-		return (0);
+		return (NULL);
 	i = 0;
-	while (s[start])
+	while (i < len)
 	{
 		str[i] = s[start];
 		i++;
@@ -33,6 +41,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 /*int	main(void)
 {
-	printf("%s", ft_substr("Arya Stark", 5, 14));
+	printf("%s\n", ft_substr("", 6, 29));
 	return (0);
 }*/
