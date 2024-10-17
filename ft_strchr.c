3f4fea32e55950b3c73bec26bf0e4c683dc8300e
daniel <daniel-congo2@hotmail.com>
2024-10-17 13:59:24 +0200

    ajout conversion en char dans ft_strchr.c et ft_strrchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daafonso <daafonso@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:16:56 by daniel149af       #+#    #+#             */
-/*   Updated: 2024/10/16 14:59:10 by daafonso         ###   ########.fr       */
+/*   Updated: 2024/10/17 13:50:58 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 char	*ft_strchr(const char *s, int c)
 {
 	size_t	i;
+	char	cc;
 
 	i = 0;
+	cc = (char)c;
 	while (s[i])
 	{
-		if (s[i] == (char)c)
+		if (s[i] == cc)
 			return ((char *)&s[i]);
 		i++;
 	}
-	if (c == '\0')
+	if (cc == '\0')
 		return ((char *)&s[i]);
 	return (NULL);
 }
@@ -31,7 +33,7 @@ char	*ft_strchr(const char *s, int c)
 /*int	main(void)
 {
 	char	str[] = "Morty";
-	int	c = 't';
+	int	c = 1024;
 	printf("%s", ft_strchr(str, c));
 	return (0);
 }*/
@@ -39,3 +41,4 @@ char	*ft_strchr(const char *s, int c)
 //Si Trouve: Return un pointeur sur le premier caractere
 //Si c == '\0' Return un pointeur vers la fin de la chaine
 //Si rien trouve return NULL
+//Conversion int en char -128 et 127 (valeurs char)
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daafonso <daafonso@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 13:41:19 by daafonso          #+#    #+#             */
-/*   Updated: 2024/10/16 17:23:20 by daafonso         ###   ########.fr       */
+/*   Updated: 2024/10/17 13:55:03 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	int		i;
+	char	cc;
 
 	i = 0;
+	cc = (char)c;
 	while (s[i])
 		i++;
 	while (i >= 0)
 	{
-		if (s[i] == (char)c)
+		if (s[i] == cc)
 			return ((char *)&s[i]);
 		i--;
 	}
@@ -38,3 +40,4 @@ char	*ft_strrchr(const char *s, int c)
 //Trouve le dernier caractere d'une string
 //Parcourir a l'envers
 //int i car i = -1 a la fin de la boucle
+//Conversion int -> char pour check -128 et 127
